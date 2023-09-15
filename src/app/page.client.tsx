a7d170dbdeb209f2b3b7ebd47e2050446d71c58d
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-15 15:17:57 +0530

    Add sample data to show

--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -15,7 +15,18 @@ const PageClient = () => {
 
   return (
     <div>
-      {employees.length === 0 ? (<button className="m-8" onClick={() => toggleAddMemberPopUp()}>Add Member</button>): 'Here the data will be shown'}
+      {employees.length === 0 ? (<button className="m-8" onClick={() => toggleAddMemberPopUp()}>Add Member</button>): (
+        employees.map((employee) => (
+          <div key={employee.id}>
+            <div>{employee.id}</div>
+            <div>{employee.email}</div>
+            <div>{employee.name}</div>
+            <div>{employee.phone_number}</div>
+            <div>{employee.position_id}</div>
+            
+          </div>
+        ))
+      )}
       {addMemberPopUp && <AddMemberPopUp toggle={toggleAddMemberPopUp} />}
     </div>
   )
