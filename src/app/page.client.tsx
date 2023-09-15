ad93f43d5f9723a11a82d76b5a320ed24ac5417d
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-09-15 17:58:43 +0530

    Delete employee

--- src/app/page.client.tsx
+++ src/app/page.client.tsx
@@ -7,10 +7,18 @@ import AddMemberPopUp from "./_components/AddMemberPopUp";
 const PageClient = () => {
 
   const employees = useStore(store => store.employees);
+  const setEmployees = useStore(store => store.setEmployees);
   const [addMemberPopUp, setAddMemberPopUp] = useState(false);
 
   const toggleAddMemberPopUp = () => {
     setAddMemberPopUp(prev => !prev);
+  }
+
+  // TODO: Make the employee_id and all other ids unique it is making multiple right now.
+
+  const removeEmployee = (employee_id: number) => {
+    const updatedArray = employees.filter((prev) => prev.id !== employee_id);
+    setEmployees(updatedArray);
   }
 
   return (
@@ -19,12 +27,15 @@ const PageClient = () => {
         <div>
           {employees.map((employee) => (
             <div key={employee.id} className="mb-8">
-              <div>Employee Id: {employee.id}</div>
-              <div>Email: {employee.email}</div>
-              <div>Name: {employee.name}</div>
-              <div>Phone Number: {employee.phone_number}</div>
-              <div>Position: {employee.position}</div>
-
+              <div>
+                <div>Employee Id: {employee.id}</div>
+                <div>Email: {employee.email}</div>
+                <div>Name: {employee.name}</div>
+                <div>Phone Number: {employee.phone_number}</div>
+                <div>Position: {employee.position}</div>
+              </div>
+              <span onClick={() => removeEmployee(employee.id)}>X</span>
+              {/* <span onClick={() => setEmployees((prevEmpArray) => prevEmpArray.filter(prev => prev.id!==employee.id))}>X</span> */}
             </div>
           ))}
         </div>
